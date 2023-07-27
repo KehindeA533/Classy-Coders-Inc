84cd793040ac2a28f76f1fa37656dbd3dcb6be48
Kehinde. A <whenkehindewalks@gmail.com>
2023-07-27 15:30:15 -0400

    update Employees

--- classes/Employees.js
+++ classes/Employees.js
@@ -1,10 +1,34 @@
 class Employees {
+  #salary;
+  #isHired;
+  #allEmployees;
+  constructor(name, postion, salary) {
+    this.name = name;
+    this.position = postion;
+    this.#salary = salary;
+    this.#isHired = true;
+  }
 
+  getSalary() {
+    return this.#salary;
+  }
 
+  setSalary(amount) {
+    this.#salary = amount;
+  }
 
-    
+  getStatus() {
+    return this.#isHired;
+  }
+  setStatus(command) {
+    if (command === "hire") {
+      this.#isHired = true;
+    } else if (command === "fire") {
+      this.#isHired = false;
+    }
+  }
 }
 
 module.exports = {
-    Employees,
-}
+  Employees,
+};
--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-const { Employees } = require("./classes/Employees")
-const { Manager } = require("./classes/Manager")
-const { SalesPerson } = require("./classes/SalesPerson")
-const { SoftwareEngineer } = require("./classes/SoftwareEngineer")
+const { Employees } = require("./classes/Employees");
+const { Manager } = require("./classes/Manager");
+const { SalesPerson } = require("./classes/SalesPerson");
+const { SoftwareEngineer } = require("./classes/SoftwareEngineer");
 
 // Run and debug your code here
 
-
-
-
-
+//
+// Employees
+//
+const preston = new Employees("Preston", "Engineer", 100000);
+console.log(preston.getSalary()); // 100000
+preston.setSalary(105000);
+console.log(preston.getSalary()); // 105000
+console.log(preston.getStatus()); // true;
+preston.setStatus("fire");
+console.log(preston.getStatus()); // false;
 
 module.exports = {
-    Employees,
-    Manager,
-    SalesPerson,
-    SoftwareEngineer
-}
+  Employees,
+  Manager,
+  SalesPerson,
+  SoftwareEngineer,
+};
